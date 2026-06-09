fa09a77c33488e0f5ed12d0b39d624c2c3cbc389
iJavaDev <yi18@live.com>
2026-06-09 04:19:13 -0500

    Update organization and tool references

--- rbx_binary/src/deserializer/state.rs
+++ rbx_binary/src/deserializer/state.rs
@@ -481,7 +481,7 @@ This may cause unexpected or broken behavior in your final results if you rely o
                                 log::warn!(
                                     "Failed to parse Attributes on {} because {:?}; falling back to BinaryString.
 
-rbx-dom may require changes to fully support this property. Please open an issue at https://github.com/UserGeneratedLLC/rbx-dom/issues and show this warning.",
+rbx-dom may require changes to fully support this property. Please open an issue at https://github.com/Do-Big-Studios/rbx-dom/issues and show this warning.",
                                     type_info.type_name,
                                     err
                                 );
@@ -504,7 +504,7 @@ rbx-dom may require changes to fully support this property. Please open an issue
                                 log::warn!(
                                     "Failed to parse MaterialColors on {} because {:?}; falling back to BinaryString.
 
-rbx-dom may require changes to fully support this property. Please open an issue at https://github.com/UserGeneratedLLC/rbx-dom/issues and show this warning.",
+rbx-dom may require changes to fully support this property. Please open an issue at https://github.com/Do-Big-Studios/rbx-dom/issues and show this warning.",
                                     type_info.type_name,
                                     err
                                 );
